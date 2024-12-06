3e4cce5286ef28f436ff07e55b497be9f07274c0
Zlatanova Irina <t0ffy@ya.ru>
2024-12-06 21:08:08 +0200

    feat: implement getAngleBetweenVectors

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -108,8 +108,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const scalarProduct = x1 * x2 + y1 * y2;
+  const vectorModulus1 = Math.hypot(x1, y1);
+  const vectorModulus2 = Math.hypot(x2, y2);
+  return Math.acos(scalarProduct / (vectorModulus1 * vectorModulus2));
 }
 
 /**
